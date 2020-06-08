fa8a4264d0eed9c69d4c93fc9a7416dea826f930
Muhammad Usama <usamamuhammad100@gmail.com>
2020-06-08 15:11:29 +0500

    EulersModified

--- test.m
+++ test.m
@@ -2,7 +2,8 @@ str = input('Give an equation in x: ','s')  ;
 x0 = input('Type in a value of inital i0: ') ; 
 y0 = input('Type in a value of inital i1: ') ; 
 h =  input('Type in a value of inital h: ') ; 
-realx = input('Type in a value of inital h: ') ; 
+realx = input('Type in a value of inital realx: ') ; 
+numberEPROX = input('Type in a value of inital EPROX: ') ;
 %itrations = input('Number of itrations : ') ; 
 
 %bisect(str,val0,val1,itrations)
@@ -10,9 +11,5 @@ realx = input('Type in a value of inital h: ') ;
 %falsePosition(str,val0 , val1, itrations)
 %newtonMethord(str,val0,itrations);
 %RangKutta4order(str,x0,y0,h,realx);
-Euler(str,x0,y0,h,realx);
-
-
-
-
-
+%Euler(str,x0,y0,h,realx);
+EulersModified(str,x0,y0,h,realx , numberEPROX);
